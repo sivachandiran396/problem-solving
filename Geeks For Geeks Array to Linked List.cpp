b06ad636ed7e5037373966cce6efe6a42aaed6b4
SIVACHANDIRAN S <165672536+sivachandiran396@users.noreply.github.com>
2024-12-17 21:17:50 +0530

    Add files via upload

---
+++ Geeks For Geeks Array to Linked List.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    // Default constructor
+    Node() {
+        data = 0;
+        next = NULL;
+    }
+
+    // Parameterised Constructor
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    Node* constructLL(vector<int>& f) {
+       Node* head=new Node(f[0]);
+    Node* mover=head;
+    for(int i=1;i<f.size();i++){
+        Node* temp=new Node(f[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> arr;
+        int num;
+        while (ss >> num) {
+            arr.push_back(num);
+        }
+        Solution ob;
+        Node* ans = ob.constructLL(arr);
+        while (ans) {
+            cout << ans->data << " ";
+            ans = ans->next;
+        }
+        cout << "\n~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
