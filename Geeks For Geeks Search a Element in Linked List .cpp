b639a160343a8f4e4c41ca49c10c67966af76faf
SIVACHANDIRAN S <165672536+sivachandiran396@users.noreply.github.com>
2024-12-17 21:20:28 +0530

    Add files via upload

---
+++ Geeks For Geeks Search a Element in Linked List .cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    // Function to count nodes of a linked list.
+    bool searchKey(int n, struct Node* head, int key) {
+        Node* temp=head;
+        while(temp!=nullptr){
+            if(temp->data==key){
+                return true;
+            }
+            temp=temp->next;
+        }
+        return false;
+    }
+};
