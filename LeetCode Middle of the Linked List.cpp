16e6b6503e03a52365fbaed4623c94baf8c2af6d
SIVACHANDIRAN S <165672536+sivachandiran396@users.noreply.github.com>
2024-12-24 18:11:03 +0530

    Add files via upload

--- LeetCode Middle of the Linked List.cpp
+++ LeetCode Middle of the Linked List.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-         int len=0;
-         ListNode* temp=head;
-         while(temp!=nullptr){
-            len++;
-            temp=temp->next;
-         }
-          int c=0;
-          while(head!=nullptr){
-            if(c==(len/2)){
-                break;
-            }
-            c++;
-            head=head->next;
-          }
-         return head;
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
     }
 };
