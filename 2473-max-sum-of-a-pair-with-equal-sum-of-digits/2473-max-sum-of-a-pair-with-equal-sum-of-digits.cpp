8e64140fb477071aa5b82e46f96354127eece248
SIVACHANDIRAN S <165672536+sivachandiran396@users.noreply.github.com>
2025-02-12 13:41:58 +0530

    Time: 28 ms (51.12%), Space: 63.8 MB (87.26%) - LeetHub

---
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int sum(int a) {
+        int n = 0;
+        while (a != 0) {
+            n += a % 10;
+            a /= 10;
+        }
+        return n;
+    }
+
+    int maximumSum(vector<int>& nums) {
+        unordered_map<int, int> sumMap;
+        int maxSum = -1;
+        for (int num : nums) {
+            int digitSum = sum(num);
+            if (sumMap.count(digitSum)) {
+                maxSum = max(maxSum, num + sumMap[digitSum]);
+            }
+            sumMap[digitSum] = max(sumMap[digitSum], num);
+        }
+
+        return maxSum;
+    }
+};
