e5f566287762e0c05d872c1f08c70cd2b41653ee
SIVACHANDIRAN S <165672536+sivachandiran396@users.noreply.github.com>
2025-06-24 07:43:44 +0530

    Time: 0 ms (100%), Space: 40.2 MB (95.27%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,18 +1,19 @@
 class Solution {
-    //these is tablulation method answer
+    public int f(int i,int j,int[][] dp){
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up=f(i-1,j,dp);
+        int left=f(i,j-1,dp);
+        return dp[i][j]=up+left;
+    }
+
+    //these is recursion with memoiazation method of answer
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0) dp[i][j]=1;
-                else{
-                int up=0,left=0;
-                if(i>0) up=dp[i-1][j];
-                if(j>0) left=dp[i][j-1];
-                dp[i][j]=up+left;
-                }
-            }
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], -1);
         }
-        return dp[m-1][n-1];
+        return f(m - 1, n - 1, dp);
     }
 }
