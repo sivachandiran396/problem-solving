f475b2bd9ce95e0256bda18b7ac7ce6e242f021f
SIVACHANDIRAN S <165672536+sivachandiran396@users.noreply.github.com>
2025-06-24 09:50:17 +0530

    Time: 0 ms (100%), Space: 41.9 MB (50.77%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,19 +1,22 @@
 class Solution {
-       public int f(int i,int j,int[][] grid,int[][] dp){
-        if(i<0 || j<0 || grid[i][j]==1) return 0;
-        if(i==0 && j==0) return 1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int up=f(i-1,j,grid,dp);
-        int left=f(i,j-1,grid,dp);
-        return dp[i][j]=up+left;
-    }
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m=obstacleGrid.length;
-        int n=obstacleGrid[0].length;
-           int[][] dp = new int[m][n];
-        for (int i = 0; i < dp.length; i++) {
-            Arrays.fill(dp[i], -1);
+        //this is tabulation type of answer
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        if(obstacleGrid[0][0]==1) return 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i==0 && j==0) dp[i][j]=1;
+                else{
+                    if(obstacleGrid[i][j]==1) continue;
+                int up=0,left=0;
+                if(i>0) up=dp[i-1][j];
+                if(j>0) left=dp[i][j-1];
+                dp[i][j]=left+up;
+                }
+            }
         }
-        return f(m - 1, n - 1,obstacleGrid, dp);
+        return dp[m-1][n-1];
     }
 }
