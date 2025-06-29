6daddfb3d31cf727b8fe42de99fabf6a11d4ad3e
SIVACHANDIRAN S <165672536+sivachandiran396@users.noreply.github.com>
2025-06-29 22:18:16 +0530

    Time: 0 ms (100%), Space: 9.5 MB (12.34%) - LeetHub

---
+++ 0889-buddy-strings/0889-buddy-strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool buddyStrings(string s, string goal) {
+        if (s.size() != goal.size()) return false;
+        if (s == goal) {
+            unordered_set<char> seen;
+            for (char c : s) {
+                if (seen.count(c)) return true;
+                seen.insert(c);
+            }
+            return false;
+        }
+        vector<int> diff;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] != goal[i]) diff.push_back(i);
+        }
+        return diff.size() == 2 &&
+               s[diff[0]] == goal[diff[1]] &&
+               s[diff[1]] == goal[diff[0]];
+    }
+};
