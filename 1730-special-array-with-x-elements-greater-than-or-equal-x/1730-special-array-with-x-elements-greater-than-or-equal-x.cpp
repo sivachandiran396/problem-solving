2cea621522e09606c5bb765b387236a1622b4c56
SIVACHANDIRAN S <165672536+sivachandiran396@users.noreply.github.com>
2025-07-04 13:54:52 +0530

    Time: 0 ms (100%), Space: 11.3 MB (29.32%) - LeetHub

---
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int specialArray(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        for (int x = 1; x <= n; x++) {
+            int count = 0;
+            for (int num : nums) {
+                if (num >= x)
+                    count++;
+            }
+            if (count == x)
+                return x;
+        }
+        return -1;
+    }
+};
