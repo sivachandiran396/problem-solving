64353e35b43c3ef9a80e245aa6cab1a4b95ae835
SIVACHANDIRAN S <165672536+sivachandiran396@users.noreply.github.com>
2025-07-13 20:26:18 +0530

    Time: 241 ms (21.35%), Space: 292.8 MB (7.91%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        if(s.size()<2) return s;
         int n=s.size();
-        vector<vector<bool>> dp(n,vector<bool>(n,false));
-        for(int i=0;i<n;i++){
-            dp[i][i]=true;
-        }
-        int start=0,maxlen=1;
-        for(int i=0;i<n-1;i++){
-            if(s[i]==s[i+1]){
-                dp[i][i+1]=true;
-                start=i;
-                maxlen=2;
-            }
-        }
-        for(int len=3;len<=n;len++){
-            for(int i=0;i<=n-len;i++){
-                int j=i+len-1;
-                if(s[i]==s[j] && dp[i+1][j-1]){
-                    dp[i][j]=true;
-                    start=i;
-                    maxlen=len;
+        string ans=s;
+        reverse(ans.begin(),ans.end());
+        vector<vector<int>> dp(n+1,vector<int> (n+1,0));
+        int end=0,maxlen=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==ans[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                    if(dp[i][j]>maxlen && (i - dp[i][j]) == (n - j)){
+                        maxlen=dp[i][j];
+                         end=i;
+                    }
                 }
             }
         }
-        return s.substr(start,maxlen);
+        string str=s.substr(end-maxlen,maxlen);
+        return str;
     }
 };
